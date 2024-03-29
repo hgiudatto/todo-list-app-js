0aacdec28402e6c155533eb2455bd76577d02f56
hgiudatto <hgiudatto@prismamp.com>
2024-03-29 16:35:39 -0300

    Todo list app

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,39 @@ import "./App.css"
 const App = () => {
   const [todo, setTodo] = useState("")
   const [todos, setTodos] = useState([])
+  const [editId, setEditId] = useState(0)
 
   const handleSubmit = (e) => {
     e.preventDefault()
 
-    /* if (editId) {
-      const editTodo = todos.find((i) => i.id === editId);
+    if (editId) {
+      const editTodo = todos.find((i) => i.id === editId)
       const updatedTodos = todos.map((t) =>
         t.id === editTodo.id
           ? (t = { id: t.id, todo })
           : { id: t.id, todo: t.todo }
-      );
-      setTodos(updatedTodos);
-      setEditId(0);
-      setTodo('');
-      return;
-    } */
+      )
+      setTodos(updatedTodos)
+      setEditId(0)
+      setTodo("")
+      return
+    }
 
     if (todo !== "") {
       setTodos([{ id: `${todo}-${Date.now()}`, todo }, ...todos])
       setTodo("")
     }
+  }
+
+  const handleDelete = (id) => {
+    const delTodo = todos.filter((to) => to.id !== id)
+    setTodos([...delTodo])
+  }
+
+  const handleEdit = (id) => {
+    const editTodo = todos.find((i) => i.id === id)
+    setTodo(editTodo.todo)
+    setEditId(id)
   }
 
   return (
@@ -39,15 +51,17 @@ const App = () => {
             value={todo}
             onChange={(e) => setTodo(e.target.value)}
           />
-          <button>Go</button>
+          <button>{editId ? "Edit" : "Go"}</button>
         </form>
 
         <ul className="allTodos">
           {todos.map((t) => (
             <li className="singleTodo">
-              <span className="todoText">{t.todo}</span>
-              <button>Edit</button>
-              <button>Delete</button>
+              <span className="todoText" key={t.id}>
+                {t.todo}
+              </span>
+              <button onClick={() => handleEdit(t.id)}>Edit</button>
+              <button onClick={() => handleDelete(t.id)}>Delete</button>
             </li>
           ))}
         </ul>
