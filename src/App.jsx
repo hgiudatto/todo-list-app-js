1793157e4014c5e1c7c0b4ebde840237de05c1ad
hgiudatto <hgiudatto@prismamp.com>
2024-04-01 11:38:10 -0300

    Rick Sanchez image

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,31 @@
 /* eslint-disable no-extra-semi */
 /* eslint-disable react/jsx-key */
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import "./App.css"
+import axios from "axios"
 
 const App = () => {
-  const [todo, setTodo] = useState("")
-  const [todos, setTodos] = useState([])
-  const [editId, setEditId] = useState(0)
+  const [rickSanchezDetails, setRickSanchezDetails] = useState()
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-
-    if (editId) {
-      const editTodo = todos.find((i) => i.id === editId)
-      const updatedTodos = todos.map((t) =>
-        t.id === editTodo.id
-          ? (t = { id: t.id, todo })
-          : { id: t.id, todo: t.todo }
+  const rickSanchez = async () => {
+    try {
+      const rickAndMortysDetail = await axios.get(
+        `https://rickandmortyapi.com/api/character`
       )
-      setTodos(updatedTodos)
-      setEditId(0)
-      setTodo("")
-      return
-    }
-
-    if (todo !== "") {
-      setTodos([{ id: `${todo}-${Date.now()}`, todo }, ...todos])
-      setTodo("")
+      console.log(rickAndMortysDetail.data.results[0])
+      setRickSanchezDetails(rickAndMortysDetail.data.results[0])
+    } catch (err) {
+      console.log(`Oops... an error occurred: ${err}`)
     }
   }
 
-  const handleDelete = (id) => {
-    const delTodo = todos.filter((to) => to.id !== id)
-    setTodos([...delTodo])
-  }
-
-  const handleEdit = (id) => {
-    const editTodo = todos.find((i) => i.id === id)
-    setTodo(editTodo.todo)
-    setEditId(id)
-  }
+  useEffect(() => {
+    rickSanchez()
+  }, [])
 
   return (
     <div className="App">
-      <div className="container">
-        <h1>Todo List App</h1>
-        <form className="todoForm" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={todo}
-            onChange={(e) => setTodo(e.target.value)}
-          />
-          <button>{editId ? "Edit" : "Go"}</button>
-        </form>
-
-        <ul className="allTodos">
-          {todos.map((t) => (
-            <li className="singleTodo">
-              <span className="todoText" key={t.id}>
-                {t.todo}
-              </span>
-              <button onClick={() => handleEdit(t.id)}>Edit</button>
-              <button onClick={() => handleDelete(t.id)}>Delete</button>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <img src={rickSanchezDetails.image} alt="" />
     </div>
   )
 }
